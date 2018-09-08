cd2319ce6916bce8e6197a2e649191e9b2674eb9
Jimmeh <jimmy.walters94@yahoo.com>
2018-09-08 16:41:55 -0500

    Add ByteColors

---
+++ ExcelsiorCore/src/main/java/ecore/services/ByteColors.java
@@ -0,0 +1,22 @@
+package ecore.services;
+
+public class ByteColors {
+
+    public static final byte WHITE = 0;
+    public static final byte ORANGE = 1;
+    public static final byte MAGENTA = 2;
+    public static final byte LIGHT_BLUE = 3;
+    public static final byte YELLOW = 4;
+    public static final byte LIME = 5;
+    public static final byte PINK = 6;
+    public static final byte GRAY = 7;
+    public static final byte LIGHT_GRAY = 8;
+    public static final byte CYAN = 9;
+    public static final byte PURPLE = 10;
+    public static final byte BLUE = 11;
+    public static final byte BROWN = 12;
+    public static final byte GREEN = 13;
+    public static final byte RED = 14;
+    public static final byte BLACK = 15;
+
+}
